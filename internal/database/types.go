1e1a0c9f756df86d17bccca5b7e434f9362457ea
Victor Ragojos <vhsvragojos@gmail.com>
2021-02-16 09:27:53 -0800

    Cut db types Collections and Databases field in DB

--- internal/database/types.go
+++ internal/database/types.go
@@ -2,16 +2,9 @@ package database
 
 import "go.mongodb.org/mongo-driver/mongo"
 
-// Collections Type
-type Collections struct {
-	Recipes *mongo.Collection
-	Users   *mongo.Collection
-}
-
 // DB Type
 type DB struct {
 	Client      *mongo.Client
 	Database    *mongo.Database
-	Databases   map[string]*mongo.Database
 	Collections map[string]*mongo.Collection
 }
