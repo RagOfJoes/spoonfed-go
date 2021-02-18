f01282aef5c5e0e404aeacd3ede84bff2025cc9f
Victor Ragojos <vhsvragojos@gmail.com>
2021-02-17 17:08:58 -0800

    Fix dataloader.UserLoader
    Create a map of passed ids to correctly return User

--- pkg/server/middlewares/dataloader.go
+++ pkg/server/middlewares/dataloader.go
@@ -21,9 +21,21 @@ func Dataloader() gin.HandlerFunc {
 		// UserLoader
 		userLoader := dataloader.NewUserLoader(dataloader.UserLoaderConfig{
 			MaxBatch: 100,
-			Wait:     1 * time.Millisecond,
+			Wait:     2 * time.Millisecond,
 			Fetch: func(ids []string) ([]*model.User, []error) {
-				return client.FindUsersByID(c.Request.Context(), ids)
+				users, errs := client.FindUsersByID(c.Request.Context(), ids)
+				if len(errs) > 0 {
+					return nil, errs
+				}
+				usersArr := []*model.User{}
+				usersMap := make(map[string]*model.User)
+				for _, user := range users {
+					usersMap[user.Sub] = user
+				}
+				for _, id := range ids {
+					usersArr = append(usersArr, usersMap[id])
+				}
+				return usersArr, nil
 			},
 		})
 		util.AddToContext(c, util.ProjectContextKeys.Dataloader, &dataloader.Loaders{
